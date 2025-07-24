ac756bf9ea10d83f0b0ef9ed31736fc5218508d6
Papp Gellert <pappgellert2003@gmail.com>
2025-07-24 16:55:41 +0300

    added animation to text, and added invisible buttons

--- script.js
+++ script.js
@@ -1,37 +1,61 @@
-const textminal = document.getElementById('textminal');
+const terminalText = document.getElementById("text_stuff");
+const terminal = document.getElementById("terminal");
+const options = document.getElementById("options");
 
 const userColor = "#89ba25";
 const suggestionColor = "#ef622a"
-
-writePromptStarter();
+const typetime = 50;
 
 setInterval(() => {
-  if (textminal.innerHTML.endsWith("&nbsp;")) {
-    textminal.innerHTML = textminal.innerHTML.slice(0, -6) + "|";
-  } else if (textminal.innerHTML.endsWith("|")) {
-    textminal.innerHTML = textminal.innerHTML.slice(0, -1) + "&nbsp"; 
+  if (terminalText.innerHTML.endsWith("&nbsp;")) {
+    terminalText.innerHTML = terminalText.innerHTML.slice(0, -6) + "|";
+  } else if (terminalText.innerHTML.endsWith("|")) {
+    terminalText.innerHTML = terminalText.innerHTML.slice(0, -1) + "&nbsp"; 
   } 
 }, 500);
 
-function writeText(strs) { 
-  textminal.innerHTML = textminal.innerHTML.slice(0, -1) + "${strs[0]}<br>";
+writePromptStarter();
+
+async function typeAnimation(elem, text, ind) {
+  console.log(text);
+  console.log(text[0]);
+  elem.innerHTML += text[ind];
+  if (ind + 1 < text.length) {
+    await new Promise(r => setTimeout(r, typetime));
+    await typeAnimation(elem, text, ind + 1);
+  } 
+}
+
+async function writeText(strs) { 
+  terminalText.innerHTML = terminalText.innerHTML.slice(0, -1) + "${strs[0]}<br>";
   for(line in strs.slice(1)) {
-    textminal.innerHTML += "> ${line}<br>"; 
+    terminalText.innerHTML += "> ${line}<br>"; 
   }
 }
 
-function writePromptStarter() {
-  let offs;
-  if (textminal.innerHTML.endsWith("&nbsp;")) {
-    offs = -6;
-  } else if (textminal.innerHTML.endsWith("|")) {
-    offs = -1;
-  } else {
-    offs = textminal.innerHTML.length;
+function removeCaret() {
+  if (terminalText.innerHTML.endsWith("&nbsp;")) {
+    terminalText.innerHTML = terminalText.innerHTML.slice(0, -6);
+  } else if (terminalText.innerHTML.endsWith("|")) {
+    terminalText.innerHTML = terminalText.innerHTML.slice(0, -1); 
   }
-  textminal.innerHTML = textminal.innerHTML.slice(0, offs) + "╭─<span style=\"color:" + userColor + "\">whois@gszp</span> ~<br>╰─$ &nbsp";
 }
 
-function animateText(base, additional) {
-
+function addCaret() {
+  terminalText.innerHTML += "|";
 }
+
+async function writePromptStarter() {
+  removeCaret();
+  //options.style.display = "none";
+  await typeAnimation(terminalText, "╭─", 0);
+  const span = document.createElement("span");
+  span.style.color = userColor;
+  terminalText.appendChild(span);
+  await typeAnimation(span, "whois@gszp", 0);
+  terminalText.innerHTML += "<br>";
+  await typeAnimation(terminalText, "╰─$", 0);
+  options.style.dispaly = "auto";
+  addCaret();
+}
+
