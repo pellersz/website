14f399c770780433121af6d1a28961c350617cff
Papp Gellert <pappgellert2003@gmail.com>
2025-07-26 13:23:29 +0300

    basically finished

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const projectOptions = document.getElementById("project-options");
 const userColor = "#89ba25";
 const suggestionColor = "#ef622a"
 //TODO: set this back to 50
-const typetime = 10;
+const typetime = 50;
+const fasttypetime = 1;
 let currentDir = "~"
 
 let currentOptions = undefined;
@@ -60,10 +61,17 @@ class focusGroup {
   }
 }
 
-async function typeAnimation(elem, text) {
+async function typeAnimation(elem, text, typet) {
   for (let i = 0; i < text.length; i++) {
     elem.innerHTML += text[i];
-    await new Promise(r => setTimeout(r, typetime));
+    if (typet) {
+      await new Promise(r => setTimeout(r, typet));
+    } else {
+      await new Promise(r => setTimeout(r, typetime));
+    }
+    if (terminal.scrollTop < (terminal.scrollHeight - terminal.offsetHeight)) {
+      terminal.scrollTop = terminal.scrollHeight;
+    }
   }
 }
 
@@ -95,7 +103,6 @@ function turnOnOptions(options) {
 }
 
 function turnOffOptions() {
-  console.log(currentOptions);
   if (currentOptions) {
     currentOptions.style.display = "none";
   }
@@ -109,22 +116,22 @@ function newLine() {
 async function writePromptStarter(cmd, options) {
   removeCaret();
   turnOffOptions();
-  await typeAnimation(terminalText, "╭─");
+  await typeAnimation(terminalText, "╭─", fasttypetime);
   const span = document.createElement("span");
   span.style.color = userColor;
   terminalText.appendChild(span);
-  await typeAnimation(span, `whois@gszp ${currentDir}`);
+  await typeAnimation(span, `whois@gszp ${currentDir}`, fasttypetime);
   terminalText.innerHTML += "<br>";
-  await typeAnimation(terminalText, `╰─$ ${cmd} `);
+  await typeAnimation(terminalText, `╰─$ ${cmd} `, fasttypetime);
   turnOnOptions(options); 
   addCaret();
 }
 
-async function writeCmds(cmds) {
+async function writeCmds(cmds, typet) {
   removeCaret();
   turnOffOptions();
   for (let i in cmds) {
-    await typeAnimation(terminalText, cmds[i]);
+    await typeAnimation(terminalText, cmds[i], typet);
     terminalText.innerHTML += "<br>";
   }
 }
@@ -158,7 +165,7 @@ async function help() {
 "███ ▄█▄ ███   ███    ███   ███    ███          ███       ███    ███   ███    ███ ███   ███   ███ ███  ███   ███   ███    ███ ███▌    ▄                        \n" + 
 " ▀███▀███▀    ██████████ ▄█████████▀          ▄████▀     ██████████   ███    ███  ▀█   ███   █▀  █▀    ▀█   █▀    ███    █▀  █████▄▄██                        \n" + 
 "                                                                      ███    ███                                             ▀                                \n\n" +
-"Hi, my name is Gellert-Szabolcs Papp, currently a masters student, who enjoys developing things.\nTo navigate click on the options bellow, or use the left and right arrows then press space or enter.\n",
+"Hi, my name is Gellert-Szabolcs Papp, currently a masters student, who enjoys developing things.\nTo navigate, click on the options bellow, or use the left and right arrows then press space or enter.\n",
     terminalText
   )
   await writePromptStarter("lets_see", currentOptions);
@@ -177,6 +184,7 @@ function neofetchLines(name_values, elem) {
 
 async function aboutMePressed() {
   turnOffOptions();
+  removeCaret();
   await writeCmds(["about_me", "neofetch"]);
   
   const container = document.createElement("div");
@@ -216,9 +224,66 @@ async function aboutMePressed() {
 
 async function projectsPressed() {
   turnOffOptions();
+  removeCaret();
   await writeCmds(["projects", "cd Projects"]);
   currentDir = "~/Projects";
   writePromptStarter("lets_see", projectOptions);
+}
+
+async function apifsPressed() {
+  turnOffOptions();
+  removeCaret();
+  await writeCmds(
+    [
+      "apifs", 
+      "cd apifs", 
+      "kitty icat apifs.png", 
+      "echo \"  apifs is a commnd line utility to create, manage reminders and notes.\\n" + 
+        "  This is a smaller personal project of mine, in an attempt to try to and learn the Rust programming language and its ecosystem. I was curious about it, because languages like C and C++ are losing trackion, while Rust is getting integrated into more and more software (like the Linux kernel) (granted, according to the Tiobe index, C and C++, are still 9 times more used, but the gap between them is slowly narrowing)\\n" + 
+        "  It can be accessed on my github page: https://github.com/pellersz/apifs.\"",
+      "cd .."
+    ],
+    fasttypetime
+  );
+  
+  const pic = document.createElement("img");
+  pic.setAttribute("src", "apifs.png");
+  terminalText.appendChild(pic);
+  displayText(
+"apifs is a commnd line utility to create, manage reminders and notes written in Rust.\n\n" + 
+"This is a smaller personal project of mine, in an attempt to try to and learn the Rust programming language and its ecosystem. I was curious about it, because languages like C and C++ are losing trackion, while Rust is getting integrated into more and more software (like the Linux kernel) (granted, according to the Tiobe index, C and C++, are still 9 times more used, but the gap between them is slowly narrowing)\n",
+    terminalText
+  );
+  terminalText.innerHTML += "The project can be accessed on my github page: <a href=\"https://github.com/pellersz/apifs\" target=\"_blank\">https://github.com/pellersz/apifs</a><br>";
+  writePromptStarter("lets_see", currentOptions);
+}
+
+async function wmpPressed() {
+  turnOffOptions();
+  removeCaret();
+  await writeCmds(
+    [
+      "Watch My Project", 
+      "cd Watch\\ My\\ Project", 
+      "kitty icat wmp.png", 
+      "echo \"  The Watch My Project softwaresysten, gives opportunity for people of the public to follow community projects and their needs. It also provides a platform for organizators to make their projects more well known.\\n" + 
+        "  This project was part of a student mentorship program, where I collaborated with other strudents. On this project I was a fullstack developer. The backend was written in JavaScript with Node.js, the frontend was created with React, the database manipulations were done with the Sequelize ORM framework.\\n" + 
+        "  We also presented the application in the 28th Transylvanian Students’ Scientific Conference, where it achieved an honorable mention.\"",
+      "cd .."
+    ],
+    fasttypetime
+  );
+  
+  const pic = document.createElement("img");
+  pic.setAttribute("src", "main_page.png");
+  terminalText.appendChild(pic);
+  displayText(
+"  The Watch My Project softwaresysten, gives opportunity for people of the public to follow community projects and their needs. It also provides a platform for organizators to make their projects more well known.\n" + 
+"  This project was part of a student mentorship program, where I collaborated with other strudents. On this project I was a fullstack developer. The backend was written in JavaScript with Node.js, the frontend was created with React, the database manipulations were done with the Sequelize ORM framework.\n" + 
+"  We also presented the application in the 28th Transylvanian Students’ Scientific Conference, where it achieved an honorable mention.", 
+    terminalText
+  );
+  writePromptStarter("lets_see", currentOptions);
 }
 
 async function back() {
